826bcc026ba7fda856e41ecf627305d4faf73106
AmryLil <ulilamry432@gmail.com>
2023-07-23 20:37:54 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html","./public/about.html"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      inter:['inter']
+    },
+  },
+  plugins: [],
+}
+
